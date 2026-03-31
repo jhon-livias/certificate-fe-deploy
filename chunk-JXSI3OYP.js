0316f2e5bdda66d811e7568ea5e7c57e71b2914d
Jhon Livias <erp@uprit.edu.pe>
2026-03-31 11:57:56 -0500

    update

--- chunk-JXSI3OYP.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-E6TGZIGP.js";var t=[{path:"login",loadComponent:()=>import("./chunk-IYBSSRPG.js").then(o=>o.LoginComponent)},{path:"",pathMatch:"full",redirectTo:"login"}];export{t as authRoutes};
